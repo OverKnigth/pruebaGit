64f3b8d5503b678687d136ae5f0adfbb7469b9ce
OverKnigth <stalin.m0012@gmail.com>
2025-09-17 18:38:21 -0500

    funciones repuerar Int y Float OK

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -7,5 +7,19 @@ recuperarTexto = function(idComponente){
 saludar = function (){
     let nombre = recuperarTexto("txtSaludo");
     let apellido = recuperarTexto("txtApellido");
-    console.log("hola " + nombre + " " + apellido);
+    let edad = recuperarInt("txtEdad");
+    let estatura = recuperarFloat("txtEstatura");
+    console.log("Hola " + nombre + " " + apellido + ", tienes " + edad + " años y mides " + estatura);
+}
+
+recuperarInt = function(idComponente){
+    let cmpValorCaja = recuperarTexto(idComponente)
+    let valorEntero = parseInt(cmpValorCaja);
+    return valorEntero;
+}
+
+recuperarFloat = function(idComponente){
+    let cmpValorCaja = recuperarTexto(idComponente)
+    let valorFloat = parseFloat(cmpValorCaja);
+    return valorFloat;
 }
