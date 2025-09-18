a4fed87636f9276c6e3524ae824ef36e7519d680
OverKnigth <stalin.m0012@gmail.com>
2025-09-18 08:26:28 -0500

    separacion de funciones al archivo utlitarios.js

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -7,35 +7,3 @@ saludar = function (){
     mostrarImagen("imgSaludo", "./img/saludo.gif");
     mostrarTextoCaja("txtNombre", "");
 }
-recuperarTexto = function(idComponente){
-    let cmpValor = document.getElementById(idComponente);
-    let valor = cmpValor.value;
-    return valor;
-}
-
-mostrarImagen = function(idComponente, rutaImg){
-    let cmpImagen = document.getElementById(idComponente);
-    cmpImagen.src = rutaImg;
-}
-
-mostrarTexto = function(idComponente, mensaje){
-    let cmpMensaje = document.getElementById(idComponente);
-    cmpMensaje.innerHTML = mensaje;
-}
-
-mostrarTextoCaja = function(idComponente, mensaje){
-    let cmpMensaje = document.getElementById(idComponente);
-    cmpMensaje.value = mensaje;
-}
-
-recuperarInt = function(idComponente){
-    let cmpValorCaja = recuperarTexto(idComponente)
-    let valorEntero = parseInt(cmpValorCaja);
-    return valorEntero;
-}
-
-recuperarFloat = function(idComponente){
-    let cmpValorCaja = recuperarTexto(idComponente)
-    let valorFloat = parseFloat(cmpValorCaja);
-    return valorFloat;
-}
