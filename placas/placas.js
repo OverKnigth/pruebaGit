047363c3271f939b458a10dbf63786615cae3a39
OverKnigth <stalin.m0012@gmail.com>
2025-09-25 20:58:44 -0500

    estilos y ejercicio funcional OK

--- placas/placas.js
+++ placas/placas.js
@@ -2,18 +2,34 @@ validarPlaca = function(){
     let placa = recuperarTexto("txtPlaca")
     let erroresEstructura = validarEstructura(placa)
     if(erroresEstructura == !false){
-        mostrarTexto("lblErrorEstructura", "Estructura Valida")
+        mostrarTexto("lblEstructuraValida", "Estructura Valida")
+        mostrarTexto("lblDatos", "Datos:")
         let provincia = obtenerProvincia(placa)
         if(provincia == null){
             mostrarTexto("lblProvincia", "Provincia Incorrecta")
         } else{
-            mostrarTexto("lblProvincia", provincia)
+            mostrarTexto("lblProvincia", "Provincia: " +provincia)
         }
         let vehiculo = obtenerTipoVehiculo(placa)
-        mostrarTexto("lblVehiculo", vehiculo)
+        mostrarTexto("lblVehiculo", " Tipo de Vehiculo: " + vehiculo)
         let dia = obtenerDiaPicoYPlaca(placa)
-        mostrarTexto("lblPicoYPlaca", dia)
+        mostrarTexto("lblPicoYPlaca", " Dia de Pico y Placa: " + dia)
     } else{
-        mostrarTexto("lblErrorEstructura", "Estructura Invalida")
+        mostrarTexto("lblEstructuraInvalida", "Estructura Invalida")
     }
+}
+
+limpiar = function(){
+    mostrarTexto("lblEstructuraValida", "")
+    mostrarTexto("lblEstructuraInvalida", "")
+    mostrarTexto("lblDatos", "")
+    mostrarTexto("lblProvincia", "")
+    mostrarTexto("lblVehiculo", "")
+    mostrarTexto("lblPicoYPlaca", "")
+    mostrarTextoEnCaja("txtPlaca", "")
+    mostrarTexto("lblError1", "")
+    mostrarTexto("lblError2", "")
+    mostrarTexto("lblError3", "")
+    mostrarTexto("lblError4", "")
+    mostrarTexto("lblError5", "")
 }
