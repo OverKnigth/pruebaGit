7a1d11d367a29da7473e0a4a340135b7785941c6
OverKnigth <stalin.m0012@gmail.com>
2025-09-28 19:35:23 -0500

    reto Aleatorio OK

--- aleatorios/aleatorios.js
+++ aleatorios/aleatorios.js
@@ -8,5 +8,31 @@ numeroAleatorio = function(){
 
 generarAleatorios = function(){
     let aleatorios = [];
-    
+    let numeroIngresado = recuperarInt("txtNumero");
+
+    if(numeroIngresado >= 5 && numeroIngresado <= 20){
+        for(let i = 0; i < numeroIngresado; i++){
+            let numero = numeroAleatorio();
+            aleatorios.push(numero);
+            console.log(i);
+            console.log(aleatorios);
+        }
+        mostrarResultados(aleatorios);
+    } else {
+        alert("El número debe estar entre 5 y 20");
+    }
+}
+
+mostrarResultados = function(arregloNumeros){
+    let cmpTabla = document.getElementById("divTabla");
+    let contenidoTabla = "<table><tr><th>Numeros Aleatorios:</th></tr>"
+    let numeros;
+    for(let i = 0; i < arregloNumeros.length; i++){
+        numeros = arregloNumeros[i];
+        contenidoTabla += "<tr><td>" 
+        contenidoTabla += numeros 
+        contenidoTabla += "</td></tr>";
+    }
+    contenidoTabla += "</table>";
+    cmpTabla.innerHTML = contenidoTabla;
 }
