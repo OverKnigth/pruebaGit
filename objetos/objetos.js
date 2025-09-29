f8a97a8ec2f5b79f3c91f7fc1f7d95e8b2f16dec
OverKnigth <stalin.m0012@gmail.com>
2025-09-29 14:17:18 -0500

    Reto crear producto OK

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -14,3 +14,28 @@ probarAtributos = function(){
         console.log("La persona esta viva");
     }
 }
+
+crearProducto = function(){
+    let producto1 = {
+        nombre: "Coca-Cola",
+        precio: 1.50,
+        stock: 100
+    }
+
+    let producto2 = {
+        nombre: "Doritos",
+        precio: 0.75,
+        stock: 100
+    }
+
+    console.log("Producto 1: " + producto1.nombre);
+    console.log("Producto 2: " + producto2.nombre);
+
+    if(producto1.stock > producto2.stock){
+        console.log("El producto 1 tiene mas stock");
+    } else if(producto2.stock > producto1.stock){
+        console.log("El producto 2 tiene mas stock");
+    } else{
+        console.log("Ambos productos tienen el mismo stock");
+    }
+}
